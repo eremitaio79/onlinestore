6155da410963ddba6ed4ea380ec8dc802f3ef1e2
Paulo de Tarso Eremita da S. Filho <paulo.filho@sead.gov>
2024-11-21 16:25:50 -0300

    Implementing home, about e controllers

---
+++ app/Http/Controllers/HomeController.php
@@ -0,0 +1,99 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class HomeController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        $viewData = [];
+        $viewData['title'] = 'Home Online Store';
+
+        return view('home.index')->with('viewData', $viewData);
+    }
+
+    public function about()
+    {
+        $data1 = "About us - Online Store";
+        $data2 = "About us";
+        $description = "This is an about page ...";
+        $author = "Developed by: Your Name";
+        return view('home.about')->with("title", $data1)
+            ->with("subtitle", $data2)
+            ->with("description", $description)
+            ->with("author", $author);
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function show($id)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function edit($id)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, $id)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy($id)
+    {
+        //
+    }
+}
